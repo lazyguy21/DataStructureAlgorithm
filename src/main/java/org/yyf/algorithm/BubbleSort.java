210e5c82cd3fdcdb59528ce66ff4eb4c74ab925c
lazyguy21 <15882041972@sina.cn>
2016-07-25 16:06:47 +0800

    冒泡排序

---
+++ src/main/java/org/yyf/algorithm/BubbleSort.java
@@ -0,0 +1,23 @@
+package org.yyf.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * Created by tobi on 16-7-25.
+ * 冒泡排序
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        Integer[] disorderArray = Constants.DISORDER_ARRAY;
+        for (int i = 0; i < disorderArray.length-1; i++) {
+            for (int j = 0; j < disorderArray.length-1-i; j++) {
+                if(disorderArray[j]<disorderArray[j+1]){
+                    int temp = disorderArray[j];
+                    disorderArray[j] = disorderArray[j + 1];
+                    disorderArray[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(disorderArray));
+    }
+}
---
+++ src/main/java/org/yyf/algorithm/Constants.java
@@ -0,0 +1,8 @@
+package org.yyf.algorithm;
+
+/**
+ * Created by tobi on 16-7-25.
+ */
+public class Constants {
+    public static final Integer[] DISORDER_ARRAY = new Integer[]{3, 4, 5, 1, 2, 6, 9, 0, 8, 7};
+}
