8c6eeadd3a9fe3cdd6858135221db57f20702de9
lazyguy21 <15882041972@sina.cn>
2016-11-15 16:30:32 +0800

    冒泡排序注释

--- src/main/java/org/yyf/algorithm/sort/BubbleSort.java
+++ src/main/java/org/yyf/algorithm/sort/BubbleSort.java
@@ -5,18 +5,22 @@ import org.yyf.algorithm.Constants;
 import java.util.Arrays;
 
 /**
- * Created by tobi on 16-7-25.
- * 冒泡排序
+ * Created by tobi on 16-7-25. 冒泡排序
+ * 每次从第一个数开始，与后面的数比较，如果前者大于后者，则交换
+ * 接着继续比较第二个数和第三个数，这样到最后一个数的时候，最后一个数放的就是最大的数
+ * 这是第一轮
+ * 接下来重复第二轮，但是末尾的结束是在到第二个数，因为刚才第一轮已经将最后一个位置放上最大数了。
+ * 重复直到第一个位置放好有序数结束
  */
 public class BubbleSort {
     public static void main(String[] args) {
         int[] disorderArray = Constants.DISORDER_ARRAY;
-        for (int i = 0; i < disorderArray.length-1; i++) {
+        for (int i = 0; i <disorderArray.length-1 ; i++) {//这里的i其实代表的是每轮循环后面不需要比较的位置数
             for (int j = 0; j < disorderArray.length-1-i; j++) {
-                if(disorderArray[j]<disorderArray[j+1]){
+                if(disorderArray[j]>disorderArray[j+1]){//如果前一个位置的数大于后一个位置，则交换
                     int temp = disorderArray[j];
                     disorderArray[j] = disorderArray[j + 1];
-                    disorderArray[j + 1] = temp;
+                    disorderArray[j+1]=temp;
                 }
             }
         }
